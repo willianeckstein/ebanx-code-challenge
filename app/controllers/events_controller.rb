85e4c971191cf11053ff59b1372c075698c1f15d
Willian Eckstein <48301841+willianeckstein@users.noreply.github.com>
2023-09-22 21:37:12 -0300

    feat: added create and update account balance methods on events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,24 @@
 class EventsController < ApplicationController
   def create 
-    puts "Create"
+    if session[create_params[:destination]].blank?
+      session[create_params[:destination]] = create_params[:amount]
+    else
+      session[create_params[:destination]] = session[create_params[:destination]] + create_params[:amount]
+    end
+
+    response = {
+      "destination" => {
+        "id" => create_params[:destination],
+        "balance" => session[create_params[:destination]]
+      }
+    }
+
+    render json: response, status: :created
+  end
+
+  private
+
+  def create_params
+    params.require(:event).permit(:type, :destination, :amount) 
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,9 @@ module EbanxCodeChallenge
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.session_store :cookie_store, key: '_ebanx_challenge_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
